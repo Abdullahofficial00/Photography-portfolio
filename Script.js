560d315993285e5e053b76531f41063be8c48483
M Abdullah <105612170+Abdullahofficial00@users.noreply.github.com>
2023-08-27 16:49:19 +0500

    photography portfolio website
    It is photography or any other portfolio website and people can use it for his own web pages or portfolio.

---
+++ Script.js
@@ -0,0 +1,15 @@
+var typed = new Typed(".text", { 
+    strings: ["Videographer", "Video Editor", "Photographer"],
+    typeSpeed: 11, 
+    backSpeed: 11,
+    backDelay: 2100, 
+    loop: true
+});
+
+var typed = new Typed(".text1", { 
+    strings: ["M Abdullah", "Kasuri"],
+    typeSpeed: 11, 
+    backSpeed: 11,
+    backDelay: 2100, 
+    loop: true
+});
